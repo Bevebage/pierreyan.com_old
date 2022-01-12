a1398a1c1fa82523cb1f56f1f4b982f47ba8dbc8
Pierre Yan <pierreyan@college.harvard.edu>
2022-01-11 22:29:42 -0500

    First working version

---
+++ components/ContainerBlock.js
@@ -0,0 +1,54 @@
+import React, { useReducer } from 'react';
+import Head from 'next/head';
+import { useRouter, userRouter } from 'next/router';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+export default function ContainerBlock({children, ...customMeta}) {
+  const router = useRouter();
+
+  const meta = {
+    title: 'Pierre Yan - Student',
+    description: 'I\'m a second-year student at Harvard University. Feel free to message me!',
+    image: '/avatar.png',
+    type: 'website',
+    ...customMeta,
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>
+          {meta.title}
+        </title>
+        <meta name='robots' content='follow, index' />
+        <meta content={meta.description} name='description' />
+        <meta
+          property='og:url'
+          content={`https://pierreyan.com${router.asPath}`}
+        />
+        <link
+          rel='canonical'
+          href={`https://pierreyan.com${router.asPath}`}
+        />
+        <meta property='og:type' content={meta.type} />
+        <meta property='og:site_name' content='Pierre Yan' />
+        <meta property='og:description' content={meta.description} />
+        <meta property='og:title' content={meta.title} />
+        <meta property='og:image' content={meta.image} />
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:site' content='@pierreyan' />
+        <meta property='twitter:title' content={meta.title} />
+        <meta property='twitter:description' content={meta.description} />
+        {meta.date && (
+          <meta property='article:published_time' content={meta.date} />
+        )}
+      </Head>
+      <main className='dark:bg-gray-800 w-full'>
+        <Navbar />
+        <div>{children}</div>
+        <Footer />
+      </main>
+    </div>
+  );
+}
---
+++ constants/data.js
@@ -0,0 +1,96 @@
+const userData = {
+  githubUsername: "Bevebage",
+  name: "Pierre Yan",
+  designation: "Second Year Student",
+  avatarUrl: "/avatar.png",
+  email: "pierreyan@college.harvard.edu",
+  phone: "+1 2674674826",
+  address: "33 chestnut st, Haddonfield\nNew Jersey, United States",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a second year student at Harvard University, I'm studying Computer Science / Molecular and Cellular Biology.",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Internship",
+      company: "DeepMap AI",
+      year: "2021",
+      companyLink: "https://www.nvidia.com/en-us/self-driving-cars/hd-mapping/",
+      desc: "Developed a three dimensional map profiling tool as a software intern. The startup got obtained by Nvidia. I didn't have any internal stock :(."
+    },
+    {
+      title: "College",
+      company: "Harvard University",
+      year: "2024",
+      companyLink: "https://www.harvard.edu/",
+      desc: "Majoring in Computer Science / Molecular and Cellular Biology. Currently a 3.8 GPA.",
+    },
+    {
+      title: "High School",
+      company: "Haddonfield Memorial High School",
+      year: "2020",
+      companyLink: "https://high.haddonfieldschools.org/",
+      desc: "Graduated from this place, that should be an achievement enough.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/pieyan13",
+    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/in/pierre-yan-5625a21b6/",
+    github: "https://github.com/bevebage",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
